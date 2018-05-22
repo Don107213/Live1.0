bc1d032f7f71ea954eaf7571d465c1eed2855343
Don107213 <Hzdong0313@buaa.edu.cn>
2018-05-22 23:25:37 +0800

    hi

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,8 +6,9 @@ Page({
    * 页面的初始数据
    */
   data: {
-    navbar:['直播','搜索','篮球','足球'],
-    currentTab:0
+    navbar: ['直播', '搜索', '生活', '体育'],
+    currentTab: 0,
+    keyboardSearchValue: ''
   },
 
   /*
@@ -17,6 +18,40 @@ Page({
     this.setData({
       currentTab: e.currentTarget.dataset.idx
     })
+  },
+
+  /**
+   * 搜索直播间
+   */
+  searchLiveRoom: function (event) {
+    var i;
+    var len = this.data.liveList.length;
+    for ( i = 0; i < this.data.liveList.length; i++) {
+      if (this.data.liveList[i].liveRoomId == parseInt(this.data.keyboardSearchValue)) {
+        wx.navigateTo({
+          url: '../live/live?id=' + this.data.liveList[i].liveRoomId,
+        })
+        wx.showToast({
+          title: "正在进入直播间",
+          duration: 1000,
+          icon: "loading"
+        })
+        break;
+      }
+    }
+    if(i==len)
+    {
+      wx.showToast({
+        title: "不存在该房间！",
+        duration: 1000,
+        image:"/images/icon/wx_app_xx.png"
+      })
+    }
+  },
+
+  bindSearchInput: function (event) {
+    var val = event.detail.value;
+    this.data.keyboardSearchValue = val;
   },
 
   /*
@@ -34,9 +69,9 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var dbLive=new DBLive();
+    var dbLive = new DBLive();
     this.setData({
-      liveList:dbLive.getAllLiveData()
+      liveList: dbLive.getAllLiveData()
     })
   },
 
@@ -44,48 +79,48 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+
   }
 })
